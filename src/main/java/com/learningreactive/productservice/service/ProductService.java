f0b9d3925be0ade559ebd27e1dc2abd57cb621bb
Sumukh <sumukhn06@gmail.com>
2022-02-04 23:09:58 +0530

    Product service logic

---
+++ src/main/java/com/learningreactive/productservice/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.learningreactive.productservice.service;
+
+import com.learningreactive.productservice.domain.ProductDomain;
+import com.learningreactive.productservice.entity.Product;
+import com.learningreactive.productservice.repository.ProductRepository;
+import com.learningreactive.productservice.util.EntityDomainUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository repository;
+
+    public Mono<ProductDomain> getById(String id){
+        return repository.findById(id)
+                .map(EntityDomainUtil::toDomain);
+    }
+    public Flux<ProductDomain> getAllProducts(){
+        return repository.findAll()
+                .map(EntityDomainUtil::toDomain);
+    }
+    public Mono<ProductDomain> insertProduct(Mono<ProductDomain> productDomainMono){
+       return productDomainMono
+               .map(EntityDomainUtil::toEntity)
+               .flatMap(product -> repository.insert(product))
+               .map(EntityDomainUtil::toDomain);
+    }
+    public Mono<ProductDomain> updateProduct(String id,Mono<ProductDomain> productDomainMono){
+        return repository.findById(id)
+                .flatMap(product -> productDomainMono
+                        .map(EntityDomainUtil::toEntity)
+                        .doOnNext(e->e.setId(id)))
+                .flatMap(repository::save)
+                .map(EntityDomainUtil::toDomain);
+    }
+    public Mono<Void> deleteProduct(String id){
+        return repository.deleteById(id);
+    }
+}
